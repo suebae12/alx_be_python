59da7e919468057f7d4c26950e5a7535a1c5adf4
Suebae <susanmarfo76@gmail.com>
2025-07-12 13:52:12 +0000

    Initial commit with python_introduction/basic_operations.py

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,36 @@
+
+# Basic Arithmetic Operations 
+
+# Addition
+
+# Subtraction
+
+# Multiplication
+
+
+#lets assign some values to variables
+
+number1 = 10
+number2 = 5
+
+# Addition
+
+result = number1 + number2
+
+print(f"Addition of 10 and 5 is {result}")
+
+
+# Subtraction
+
+result = number1 - number2
+
+print(f"Subtraction of 10 and 5 is {result}")
+
+
+
+# Multiplication
+
+result = number1 * number2
+
+print(f"Multiplication of 10 and 5 is {result}")
+
