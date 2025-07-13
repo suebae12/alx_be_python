4e76f3a74a06ca1fc1da9ab23868115d5114a30b
Suebae <susanmarfo76@gmail.com>
2025-07-13 16:50:45 +0000

    Fix error in hours_to_seconds script

--- python_introduction/hours_to_seconds.py
+++ python_introduction/hours_to_seconds.py
@@ -1,10 +1,11 @@
-# Assign variables
-# 3600s =1h
 
-# Define number of hours
+# hours_to_seconds.py
+
+# Define the number of hours
 hours = 2
 
+# Calculate the number of seconds
+seconds = hours * 3600
 
-seconds = 2 * 3600
-
-print(f"{hours} hour(s) is {seconds} seconds")
+# Display the result
+print(f"{hours} hour(s) is {seconds} seconds.")
