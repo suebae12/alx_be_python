1c0fc8c551468cb8397482a80c0ed0bcb48b5377
Suebae <susanmarfo76@gmail.com>
2025-07-14 06:24:58 +0000

    Add the script containing the finance calculator script

---
+++ python_introduction/finance_calculator.py
@@ -0,0 +1,7 @@
+#ask user to input their monthly salary
+income = float(input("Enter your monthly income:"))
+expenses = float(input("Enter your total monthly expenses:"))
+
+#calculate monthly savings
+savings = income - expenses
+
